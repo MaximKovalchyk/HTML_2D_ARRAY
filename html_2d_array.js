a9f83f60aa3fcef19f77d50331a832493e1ff99c
Maksym <ko.maxima@gmail.com>
2015-03-26 19:39:52 +0200

    add methods interfaces

--- html_2d_array.js
+++ html_2d_array.js
@@ -1,3 +1,34 @@
+/*
+	constructor
+	parametersObj : {
+		container_HTML_Node
+		//------------------
+		callback_function
+		//------------------
+		values_2d_array // 2d array with values for class object like: [[1,2], [4,5], [7,8]]
+		//------------------
+		//ignored, if values_2d_array is present
+		x_size 
+		y_size
+		//------------------
+		ROW_HTML_TAG_NAME
+		CELL_HTML_TAG_NAME
+	}
+*/
 function HTML_2D_ARRAY(parametersObj) {
-    
+	
+}
+
+/*
+	get value of cell
+*/
+HTML_2D_ARRAY.prototype.get_value = function(x, y) {
+
+}
+
+/*
+	set value of cell
+*/
+HTML_2D_ARRAY.prototype.set_value = function(x, y, value) {
+
 }
